941a4fae2ca59ef909cb427b71663d52912ef4c5
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2022-08-05 03:19:01 -0300

    refactor: challenge finished

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  ReactNode,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps{
+  name: string,
+  icon?: ReactNode, 
+  [x: string]: any
+}
+
+export function Input({ name, icon,...rest}:InputProps){
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -37,7 +44,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {icon && <Icon size={20} />} */}
 
       <input
         onFocus={handleInputFocus}
@@ -50,4 +57,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   );
 };
 
-export default Input;
---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import { Input } from '../Input';
+import { FoodProps } from '../Food';
+
+interface ModalAddFoodProps {
+  setIsOpen: () => void,
+  handleAddFood: (food: FoodProps) => void,
+  isOpen: boolean
+}
+
+export function ModalAddFood({ handleAddFood, setIsOpen, isOpen }: ModalAddFoodProps) {
+  const formRef = useRef(null)
+
+  function handleSubmit(data: FoodProps) {
+    console.log(data)
+    handleAddFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
